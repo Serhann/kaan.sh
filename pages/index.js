366d96a93a21cf7a3c29c50628f7fb7bca001eb9
Kaan Mutlu <kaan@kaanmutlu.com.tr>
2021-03-26 12:01:37 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -73,8 +73,6 @@ export default function Home() {
                       strokeLinejoin="round"
                     >
                       <path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.777-7.777zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4" />
-                      <rect x="2" y="9" width="4" height="12" />
-                      <circle cx="4" cy="4" r="2" />
                     </g>
                   </svg>
                 </ExternalLink>
