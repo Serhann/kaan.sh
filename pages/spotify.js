78d75f515cf5f157c58cc6e21fc4377553ab9f9f
Kaan Mutlu <kaan@kaanmutlu.com.tr>
2021-03-26 16:22:26 +0100

    Update spotify.js

--- pages/spotify.js
+++ pages/spotify.js
@@ -1,15 +1,7 @@
-import { useLastFM } from 'use-last-fm';
-
-export default function CurrentlyPlaying() {
-  const lastFM = useLastFM('kaanmutlu', 'd5f4757d3e893690cac17f3579841f33');
-
-  if (lastFM.status !== 'playing') {
-    return <p>Not listening to anything</p>;
-  }
-
-  return (
-    <p>
-      Listening to {lastFM.song.name} by {lastFM.song.artist}
-    </p>
-  );
-};
+<div class="flex items-center space-x-5 border-2 w-96 border-black h-24 rounded-2xl">
+  <img src={lastFM.song.art} class="rounded-lg ml-4 w-16 h-16" />
+  <div class="flex flex-col">
+    <h1>Listening to <strong>{lastFM.song.name}</strong> by <strong>{lastFM.song.artist}</strong></h1>
+    <h1 class="text-sm">Spotify</h1>
+  </div>
+</div>
