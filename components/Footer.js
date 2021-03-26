6f541017a0233961342c7e9424a8b429d695de42
Kaan Mutlu <kaan@kaanmutlu.com.tr>
2021-03-26 18:06:09 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -11,8 +11,9 @@ const ExternalLink = ({ href, children }) => (
 
 export default function Footer() {
   return (
+    <script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" crossorigin="anonymous" />
     <footer className="mb-8 text-gray-600 dark:text-gray-400 flex flex-col items-center">
-      <p><small>Made with <img src="heart.png" width="18" height="18" /> by Kaan using Next.JS and TailwindCSS.</small></p>
+      <p><small>Made with <i class="twa twa-black-heart" /> by Kaan using Next.JS and TailwindCSS.</small></p>
     </footer>
   );
 }
