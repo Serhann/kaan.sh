e2f288c2d10e25ac887231280be7f92c9e38b55e
Kaan Mutlu <kaan@kaanmutlu.com.tr>
2021-03-26 16:55:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -102,8 +102,8 @@ export default function Home() {
               CEO & Founder of Sanvia Digital | Software Developer
               </h2>
     
-    <div class="mb-4 flex items-center space-x-5 border-2 w-96 border-black h-24 rounded-2xl">
-        <img src="{lastFM.song.art}" class="rounded-lg ml-4 w-16 h-16" />
+    <div class="mb-10 flex items-center space-x-5 border-2 w-96 border-black h-24 rounded-2xl">
+        <img src={lastFM.song.art} class="rounded-lg ml-4 w-16 h-16" />
         <div class="flex flex-col">
           <h1 class="text-black">Listening to <strong>{lastFM.song.name}</strong> by <strong>{lastFM.song.artist}</strong></h1>
           <h1 class="text-sm text-black"><span><img src="spotify.svg" width="12" height="12" /> Spotify</span></h1>
