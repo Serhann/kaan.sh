575515ef5d7d9574d30b8031dd828da4f87ad8b2
Kaan Mutlu <kaan@kaanmutlu.com.tr>
2021-03-27 22:19:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -97,6 +97,8 @@ export default function Home() {
               CEO & Founder of Sanvia Digital | Software Developer
               </h2>
 
+    <iframe src="https://spotifypresence.armagan.rest/embed?id=524681391959441419" width="768" height="71" frameborder="0" allowTransparency="true">
+    
               <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
                 About
               </h1>
